30f9c7fd8ce5b2d59eb572d16e2aae62eb926ea0
Ruta Klimaviciute <rutaklim7@gmail.com>
2025-03-15 20:20:15 +0000

    keypress events

--- section-three/02-modal-window/script.js
+++ section-three/02-modal-window/script.js
@@ -5,15 +5,15 @@ const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const openModalBtn = document.querySelectorAll('.show-modal');
 
-// Close modal
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
 // Open modal
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+};
+// Close modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 for (let i = 0; i < openModalBtn.length; i++) {
   openModalBtn[i].addEventListener('click', openModal);
@@ -21,3 +21,9 @@ for (let i = 0; i < openModalBtn.length; i++) {
 
 closeModalBtn.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+// keypress event
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
