117ab229c8b095ace7ed2b5df35652f5b9f08949
Ruta Klimaviciute <rutaklim7@gmail.com>
2025-04-09 21:26:42 +0100

    this, and functions

--- section-4/script.js
+++ section-4/script.js
@@ -29,6 +29,7 @@ function calcAge(birthYear) {
 const firstName = "Ruta";
 calcAge(2006);
 */
+/*
 // ****************************
 // #2 HOISTING AND TDZ
 // ****************************
@@ -68,3 +69,91 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+/*
+// ****************************
+// #3 'THIS' KEYWORD
+// ****************************
+// console.log(this);
+const calcAge = function (birthYear) {
+  console.log(2025 - birthYear);
+  // console.log(this);
+};
+calcAge(2006);
+//
+const calcAgeArrow = (birthYear) => {
+  console.log(2025 - birthYear);
+  // console.log(this);
+};
+calcAgeArrow(2006);
+//
+const ruta = {
+  year: 2006,
+  calcAge: function () {
+    console.log(this);
+    console.log(2025 - this.year);
+  },
+};
+ruta.calcAge();
+//
+const tommy = {
+  year: 2015,
+};
+tommy.calcAge = ruta.calcAge;
+tommy.calcAge();
+
+const f = ruta.calcAge;
+//
+const pencil = {
+  books: 27,
+  pencils: 6,
+  totPencils: function () {
+    return this.pencils * 3;
+  },
+  totBooks: function () {
+    return this.books * 2;
+  },
+};
+console.log(pencil.totBooks());
+console.log(pencil.totPencils());
+*/
+
+// ****************************
+// #4 REGULAR FUNCTIONS VS ARROW FUNCTIONS
+// ****************************
+const ruta = {
+  firstName: "Ruta",
+  year: 2006,
+  calcAge: function () {
+    console.log(this);
+    console.log(2025 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const IsMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+    // Solution 2
+    const IsMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    IsMillenial();
+  },
+  greet: () => console.log(`Hey ${this.firstName}`), // doesn't work
+  bye: function () {
+    console.log(`Bye ${this.firstName}!!`);
+  }, // works
+};
+ruta.greet(); // doesn't work
+ruta.bye(); // works, so just use a normal function
+ruta.calcAge();
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => a + b;
