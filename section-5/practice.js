6962fa22c38ba9fb8e006edb6f0693442c6c77a4
Ruta Klimaviciute <rutaklim7@gmail.com>
2025-04-25 22:19:35 +0100

    destructuring an array

---
+++ section-5/practice.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const ruta = {
+  name: 'Ruta',
+  age: 18,
+  hobbies: ['reading', 'eating', 'watching series', 'music'],
+};
+/*
+// Destructuring an array - making multiple variables:
+const [first, , third] = ruta.hobbies;
+console.log(first, third);
+*/
